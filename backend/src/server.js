21ba031de0b01ac18dbba39a17cc2ff618c664a9
Anshul Mukati <anshulmukati78@gmail.com>
2025-12-28 23:05:51 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,7 @@ const userRoutes = require("./routes/user.router.js");
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:5173",  
+    origin: "https://mechlink-frontend.onrender.com",  
     credentials: true,              
   })
 );
