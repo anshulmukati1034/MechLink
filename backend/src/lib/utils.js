9a77d0526bcb431dea1f9ea21cd179017bf368ad
Anshul Mukati <anshulmukati78@gmail.com>
2025-12-29 16:43:19 +0530

    Update utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -7,9 +7,9 @@ const generateToken = (userId, res) => {
 
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, 
-    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-    secure: process.env.NODE_ENV === "production", // only send cookie over HTTPS in production
+    httpOnly: true,
+    sameSite: "none",   
+    secure: true,  
   });
 
   return token;
