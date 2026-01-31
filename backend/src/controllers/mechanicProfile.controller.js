be4bb65aa3abbd4b40202dbf62f9fe1c5c37b06b
Anshul Mukati <anshulmukati78@gmail.com>
2026-01-31 22:52:36 +0530

    Update mechanicProfile.controller.js

--- backend/src/controllers/mechanicProfile.controller.js
+++ backend/src/controllers/mechanicProfile.controller.js
@@ -1,3 +1,44 @@
+// const db = require("../models/model.js");
+// const Mechanic = db.Mechanic;
+// const MechanicProfile = db.MechanicProfile;
+
+
+// exports.getMechanicProfile = async (req, res) => {
+//   try {
+//     const { mechanicId } = req.body;
+
+//     if (!mechanicId) {
+//       return res.status(400).json({
+//         message: "mechanicId is required",
+//       });
+//     }
+
+//     const mechanic = await MechanicProfile.findOne({
+//       where: { Mechanic_Id: mechanicId },
+//       include: [
+//         {
+//           model: Mechanic,
+//         },
+//       ],
+//     });
+
+//     if (!mechanic) {
+//       return res.status(404).json({
+//         message: "Mechanic not found",
+//       });
+//     }
+
+//     res.status(200).json({
+//       success: true,
+//       data: mechanic,
+//     });
+//   } catch (error) {
+//     console.error("Profile API error:", error);
+//     res.status(500).json({
+//       message: "Server error",
+//     });
+//   }
+// };
 const db = require("../models/model.js");
 const Mechanic = db.Mechanic;
 const MechanicProfile = db.MechanicProfile;
@@ -39,4 +80,5 @@ exports.getMechanicProfile = async (req, res) => {
     });
   }
 };
+
 
