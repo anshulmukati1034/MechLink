ee58d9b7f50054cc6843b982f335bbff13f6a0c6
Anshul Mukati <anshulmukati78@gmail.com>
2026-02-07 22:15:38 +0530

    Update utils.js
    Uncommented the JWT token generation code and its export.

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,3 +1,23 @@
+// const jwt = require("jsonwebtoken");
+
+// const generateToken = (userId, res) => {
+//   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+//     expiresIn: "7d",
+//   });
+
+//   res.cookie("jwt", token, {
+//     maxAge: 7 * 24 * 60 * 60 * 1000, 
+//     httpOnly: true,
+//     sameSite: "none",   
+//     secure: true,  
+//   });
+
+//   return token;
+// };
+
+// module.exports = generateToken;
+
+
 const jwt = require("jsonwebtoken");
 
 const generateToken = (userId, res) => {
